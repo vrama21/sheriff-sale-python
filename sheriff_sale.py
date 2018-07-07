635864e81700e2fb951caede1531d611798b37e8
vrama21 <vrama21@gmail.com>
2018-07-07 02:32:37 -0400

    Added missing cities and suffixes to the constants. Search functionality is now working. Returns a cleaned, edited tuple of ('Address', 'City', 'Zip Code') May still need some refactoring.

--- sheriff_sale.py
+++ sheriff_sale.py
@@ -24,64 +24,46 @@ class SheriffSale:
         return str(self.address_data)
 
     def run_selenium(self):
-        sale_dates = []
-        table_data = []
-        table_addr = []
-
         driver = webdriver.Chrome()
         driver.get(sheriff_sales_url)
         driver.maximize_window()
 
+        # Find date drop down, grab the text
         date = driver.find_elements_by_xpath('//select[@id="PropertyStatusDate"]/option')
-        for dates in date:
-            sale_dates.append(dates.get_attribute('value'))
-
-        sale_dates.pop(0)
-        driver.find_element_by_xpath('//select[@id="PropertyStatusDate"]/option[@value="{}"]'.format(sale_dates[1])).click()
+        sales_date = [dates.get_attribute('value') for dates in date]
+        sales_date.pop(0)   # Remove initial blank date
+        sales_date_xpath = '//select[@id="PropertyStatusDate"]/option[@value="{}"]'
+        driver.find_element_by_xpath(sales_date_xpath.format(sales_date[0])).click()
         driver.find_element_by_css_selector("[type=submit]").click()
 
         page_source = driver.page_source
-        _html = BeautifulSoup(page_source, 'html.parser')
+        html = BeautifulSoup(page_source, 'html.parser')
 
-        for row in _html.find_all('tr'):
+        # html_tr = [row for row in html.find_all('tr')]
+
+        table_data = []
+        for row in html.find_all('tr'):
             table_data.append([td.text for td in row.find_all('td')])
 
-        for i in table_data[1:]:
-            table_addr.append(i[5])
         driver.close()
 
         # Cleanup
+        table_addr = [data[5] for data in table_data[1:]]
         for key, value in replace_dict.items():
             table_addr = [re.sub(r'\b({})\b'.format(key), value, x) for x in table_addr]
 
-        regex_by_city = re.compile(r'|'.join(city_list))
-        # regex_by_street_suffix = re.compile(r'\b|\b'.join(street_suffix))
-        regex_by_street_suffix = re.compile(r"(.+)\s*(" + r'\b|\b'.join(street_suffix) + r")\s*(.+)")
-        print(regex_by_street_suffix)
+        # Search
+        regex_street_suffix = re.compile(r'(.*(?:' + r'|'.join(street_suffix) + r'))+\s(.*)+')
+        regex_city = re.compile(r'|'.join(city_list))
+        regex_zip_code = re.compile(r'\d{5}')
 
-        # a = [re.split(regex_by_street_suffix, row) for row in table_addr]
-        a = [re.search(regex_by_street_suffix, row).groups() for row in table_addr]
-        address = [''.join(x[0:2]) for x in a]
-        for i in address:
+        street_match = [regex_street_suffix.findall(row)[0][0] for row in table_addr]
+        city_match = [regex_city.findall(row)[0] for row in table_addr]
+        zip_match = [regex_zip_code.findall(row)[0] for row in table_addr]
+
+        result = list(zip(street_match, city_match, zip_match))
+        for i in result:
             print(i)
-
-
-
-        # for row in table_addr:
-        #     initial_split = row.split('NJ')
-        #     address_0 = initial_split[0]
-        #     zip_code = initial_split[-1]
-        #
-        #     address = re.split(regex_by_city, str(address_0))
-        #     address = [x.split(' (') for x in address]
-        #     # a2 = [x.split(' )') for x in a1[1]]
-        #
-        #     # print(initial_split)
-        #     print(address[0][0])
-        #     print(zip_code)
-        # # return self.address_data
-
 
 if __name__ == "__main__":
     main = SheriffSale()
-    print(main)
