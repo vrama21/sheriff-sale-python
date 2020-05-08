de7701c699e9a659ad1643f2313d9bfdf3d7e2b0
Valon Rama <vrama21@gmail.com>
2020-05-08 11:33:33 -0400

    Added an isLoading state to the useFetch hook

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -3,22 +3,25 @@ import { useState, useEffect } from 'react';
 const useFetch = (url, options = {}) => {
     const [response, setResponse] = useState(null);
     const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const resp = await fetch(url, options);
                 const json = await resp.json();
+                setIsLoading(true);
                 setResponse(json);
             } catch (error) {
                 setError(error);
             }
         };
 
+        setIsLoading(false);
         fetchData();
     }, []);
 
-    return { response, error };
+    return { response, error, isLoading };
 };
 
 export default useFetch;
