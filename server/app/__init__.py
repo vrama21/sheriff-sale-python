d16fb491b9bbdb4247c9a6138a06aa99812957b8
Valon Rama <vrama21@gmail.com>
2020-06-07 00:53:05 -0400

    Added logging functionality

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,6 +1,23 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 CORS(app)
@@ -8,6 +25,7 @@ CORS(app)
 app.config["SECRET_KEY"] = "17a202d87bb99ee84fd5ebbec5130e0f"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 db = SQLAlchemy(app)
 
