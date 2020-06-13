8f6a5b4a39ae9a63676444d557dd8330f7bee49e
Valon Rama <vrama21@gmail.com>
2020-06-13 14:45:07 -0400

    Removed the zillow key. Also added diff zillow library

--- server/app/scrapers/zillow.py
+++ server/app/scrapers/zillow.py
@@ -1,4 +1,4 @@
-import zillow
+from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults
 from pathlib import Path
 from ..utils import BASE_DIR
 
@@ -6,12 +6,12 @@ from ..utils import BASE_DIR
 def test():
     path = Path(BASE_DIR, 'config/zillow_key.conf')
     with open(path, 'r') as f:
-        key = f.readline().replace("\n", "")
+        key = f.readline().rstrip().replace("\n", "")
 
-    api = zillow.ValuationApi()
+    print(key)
+    zillow_data = ZillowWrapper(key)
 
-    address = "574 Revere Way Galloway Township NJ"
-    zip_code = "08205"
-
-    data = api.GetSearchResults(key, address, zip_code)
-
+    response = zillow_data.get_deep_search_results(address='574 Revere Way', zipcode='08205')
+    # response = zillow_data.get_updated_property_details('37807220')
+    result = GetDeepSearchResults(response)
+    return result
