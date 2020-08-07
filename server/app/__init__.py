85a170b88d8e0cf622c92c496497742d3093cf24
Valon Rama <vrama21@gmail.com>
2020-08-07 01:36:11 -0400

    The database will create any non-existing tables from the models on intiailization

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -15,5 +15,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+db.create_all()
+db.session.commit()
 
 from . import routes
