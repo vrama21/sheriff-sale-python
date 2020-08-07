88f54086ce5fed1b2b21dbc2829189cacbccedaf
Valon Rama <vrama21@gmail.com>
2020-08-07 01:46:19 -0400

    Added a CountyClerk db model

--- server/app/models.py
+++ server/app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from app import db
+from . import db
 
 
 class SheriffSaleDB(db.Model):
@@ -28,15 +28,40 @@ class SheriffSaleDB(db.Model):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
     def __repr__(self):
-        return f"""SheriffSaleDB(''{self.sale_date}', '{self.judgment}',
-        '{self.address_sanitized}', '{self.unit}', '{self.city}', '{self.zip_code}',
-        '{self.sheriff}'"""
+        return
+        f"""
+            SheriffSaleDB(''{self.sale_date}', '{self.judgment}',
+            '{self.address_sanitized}', '{self.unit}', '{self.city}', '{self.zip_code}',
+            '{self.sheriff}'
+        """
 
 
 class NJParcelsDB(db.Model):
     __tablename__ = "NJParcels"
     id = db.Column("id", db.Integer, primary_key=True)
-    city = db.Column("City", db.String(60))
     block = db.Column("Block", db.Integer())
     address = db.Column("Address", db.String(100))
+    city = db.Column("City", db.String(60))
     lot = db.Column("Lot", db.String(15))
+
+
+class CountyClerkDB(db.Model):
+    __tablename__ = 'CountyClerk'
+    id = db.Column("id", db.Integer, primary_key=True)
+    block = db.Column("block", db.String)
+    correction_flag = db.Column("correction_flag", db.String)
+    cross_party_name = db.Column("cross_party_name", db.String)
+    doc_id = db.Column("doc_id", db.String, unique=True)
+    doc_status = db.Column("doc_status", db.String)
+    doc_type = db.Column("doc_type", db.String)
+    file_num = db.Column("file_num", db.String)
+    legal_1 = db.Column("legal_1", db.String)
+    lot = db.Column("lot", db.String)
+    party_code = db.Column("party_code", db.String)
+    party_name = db.Column("party_name", db.String)
+    pv_no_data = db.Column("pv_no_data", db.String)
+    rec_date = db.Column("rec_date", db.String)
+    rowid = db.Column("rowid", db.String)
+    town = db.Column("town", db.String)
+
+
