daea3dd329ccbb35a05f7173d2c6e126cc603c91
Valon Rama <valon.rama@creditly.com>
2020-12-16 01:51:50 -0500

    Fixed API bug because it was pointing to wrong DB

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.config["SECRET_KEY"] = "17a202d87bb99ee84fd5ebbec5130e0f"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgressql:///main.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
