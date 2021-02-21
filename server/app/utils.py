56cf440f622b8b78712aa576b4f1f5b269ede5f7
Valon Rama <valon.rama@creditly.com>
2021-02-20 21:57:23 -0500

    utils cleanup

--- server/app/utils.py
+++ server/app/utils.py
@@ -5,21 +5,37 @@ from bs4 import BeautifulSoup
 
 
 def requests_content(url: str, session: requests.Session = None):
-    """ Creates an html request session and returns the BeautifulSoup parse
-    :param url: The url to fetch the request from
-    :param session: If a session already exists, update the request in the current session
-    """
-    response = session.get(url) if session else requests.get(url)
-    content = response.content
-    soup = BeautifulSoup(content, "html.parser")
+    """ 
+    Creates an html request session and returns the BeautifulSoup parse
 
-    return soup
+    Parameters:
+        url: The url to fetch the request from
+        session: If a session already exists, update the request in the current session
+
+    Returns:
+        A beautifulsoup object of the request
+    """
+    try:
+        response = session.get(url) if session else requests.get(url)
+        content = response.content
+        soup = BeautifulSoup(content, "html.parser")
+
+        return soup
+    except ConnectionError as err:
+        raise ConnectionError('Cannot Access URL: ', err)
 
 
 def load_json_data(json_path: str):
-    """ Reads and loads a specified json file.
-    :param json_path: The path arg follows the format of app/*json_path*
+    """ 
+    Reads and loads a specified json file.
+
+    Parameters:
+        json_path: The path arg follows the format of app/*json_path*
+    
+    Returns:
+        A json object
     """
+
     path = Path(__file__).resolve().parent / json_path
 
     with open(path, "r") as file:
