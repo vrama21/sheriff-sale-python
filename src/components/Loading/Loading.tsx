d9da649c635b16b1c02415b683e81faedafba5db
Valon Rama <valon.rama@creditgenie.com>
2021-04-19 23:31:47 -0400

    Added a LoadingSpinner svg

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ReactComponent as LoadingSVG } from '../../assets/loading_spinner.svg';
+import { LoadingStyles } from './Loading.style';
+
+const LoadingSpinner: React.FC = () => {
+  const classes = LoadingStyles();
+
+  return (
+    <div className={classes.root}>
+      <LoadingSVG style={{ background: undefined }} />
+    </div>
+  );
+};
+
+export default LoadingSpinner;
